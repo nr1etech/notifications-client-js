b5619fc39eee02f46a5e27eef3d4cc9e4eeeb009
Jake Roberts <113054933+jroberts-logicl@users.noreply.github.com>
2022-09-21 16:49:23 -0600

    Added a check for the NPM token before proceeding with a publish.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+if [[ -z "$NPM_AUTH_TOKEN" ]]; then
+	echo "NPM_AUHT_TOKEN is not set. Publish aborted." >&2
+	exit 1;
+fi
+
 ./scripts/clean-artifacts.sh
 ./scripts/build.sh
 
